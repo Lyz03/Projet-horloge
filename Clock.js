5626ee3073d2693c4a73f63f3d8319e9210f550f
Lyz03 <lizoe@netcourrier.com>
2021-12-14 09:46:54 +0100

    basic clock

--- Clock.js
+++ Clock.js
@@ -1,14 +1,22 @@
 let Clock = function (secondsDiv, minutesDiv, hoursDiv) {
 
     this.showTime = function () {
-        console.log('test')
-        setInterval(function () {
+        setInterval(function (){
             this.date = new Date;
+
             this.sec = this.date.getSeconds();
+            this.min = this.date.getMinutes();
+            this.h = this.date.getHours();
+
+            if (this.h > 12) {
+                this.h -= 12;
+            }
+
             this.secDeg = (this.sec * 6) + 90;
-            this.minDeg = (this.sec * 0.1) + 90;
-            this.hDeg = (this.sec * 0.0083333) +90;
-            secondsDiv.style.transform = " translate(50px, 250px) rotate("+ this.secDeg.toString() +"deg)";
+            this.minDeg = (this.min * 6) + 90;
+            this.hDeg = (this.h * 30) +90;
+
+            secondsDiv.style.transform = " translate(70px, 250px) rotate("+ this.secDeg.toString() +"deg)";
             minutesDiv.style.transform = " translate(90px, 250px) rotate("+ this.minDeg.toString() +"deg)";
             hoursDiv.style.transform = " translate(120px, 250px) rotate("+ this.hDeg.toString() +"deg)";
         }, 1000)
